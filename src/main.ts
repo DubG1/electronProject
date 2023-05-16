20af2ed8db48bbbfcf6a9d55bc340bc8522bddc9
doubleg <georg.gas@live.at>
2023-05-17 00:40:43 +0200

    added vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,32 @@ import "./style.css"
 import App from './App.vue'
 import './samples/node-api'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const themeConfigs = {
+    dark: false,
+    colors: {
+        primary: '#449A8B'
+    }
+}
+
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'themeConfigs',
+        themes: {
+            themeConfigs,
+        }
+    }
+})
+
 createApp(App)
+.use(vuetify)
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')
